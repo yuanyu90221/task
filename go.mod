7ee6dfc0beb3523bf8529cea8eddd46f204cc2c9
jsonLiang <yuanyu90221@gmail.com>
2020-07-20 00:22:55 +0800

    add list

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/yuanyu90221/task
 go 1.14
 
 require (
+	github.com/boltdb/bolt v1.3.1
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/spf13/afero v1.3.2 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/yuanyu90221/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/yuanyu90221/task/cmd"
+	"github.com/yuanyu90221/task/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
